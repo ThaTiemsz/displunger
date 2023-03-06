5eac9a060df4912ed25603636a91d505ae1b46be
Tiemen <Tiemen2@live.nl>
2023-03-06 18:54:36 +0100

    2.0.0
    * add fallback for fetching builds
    * add cli arguments with overriding support
    * proxy api requests to fix cors issues

--- README.md
+++ README.md
@@ -1,21 +1,56 @@
 ![displunger banner](https://cdn.discordapp.com/attachments/415951527258095616/822453386263265290/displunger.banner.png)
 
-view/run old discord builds.
+View/run old Discord builds.
 
-### How to use
+## How to use
 
-Run `launch.bat` or use `node displunger` to launch the web app.
-Visit `http://localhost:3000/launch/<BUILD HASH>` to view a build.
+1. Run `pnpm install` to install the dependencies.
+2. Run `node displunger` to launch the web app.
+3. Visit `http://localhost:3000/launch/<BUILD HASH>` to view a build.
 
 ## Warning
 
-I'd advise you to only run this on phone verified accounts, as discord might lock you out/force you to verify your phone number due to old api requests.
+I'd advise you to only run this on phone verified accounts, as Discord might lock you out/force you to verify your phone number due to old API requests.
 
-### Old Builds
+## CLI Options
+
+### `--port` `-p`
+
+This option will set the port the web app will run on. Defaults to `3000`.
+
+### `--host` `-h`
+
+This option will set the host the web app will run on. Defaults to `http://127.0.0.1:PORT`.
+
+### `--science` `-s`
+
+This option will set the release channel to Staging and enables access to the experiments tab. Defaults to false.
+
+### `--js` `-j`
+
+This option will manually set the root JS scripts in case the build data could not be fetched.
+
+**Usage:**\
+`node displunger -j <filename>,<filename>,<filename>,<filename>`\
+or\
+`node displunger -j <filename> -j <filename> -j <filename> -j <filename>`
+
+### `--css` `-c`
+
+This option will manually set the root CSS file in case the build data could not be fetched.
+
+### `--api` `-a`
+
+This option will manually set the API version in case the build data could not be fetched.
+
+**Usage:**
+`node displunger -c <filename>`
+
+## Old Builds
 The oldest build we can currently launch is `21837 (10b6c00342136bb3324d2d694b86050ae915017b)`.
 Really old builds (roughly early 2018) have since been unpublished and cannot be loaded anymore.
 
 Many older builds fail to function properly due to breaking changes made on the API.
 
-### Credits
-powered by [Megumin's Discord Build Archive](https://discord.sale)
+## Credits
+Powered by [Megumin's Discord Build Archive](https://discord.sale) and [Discord Build Logger](https://github.com/Discord-Build-Logger/Builds)
--- displunger.js
+++ displunger.js
@@ -1,146 +1,249 @@
-const express = require('express');
-const superagent = require('superagent');
-const fs = require('fs');
-const { env } = require('process');
+/**
+ * Partial source code from https://gitlab.com/derpystuff/displunger
+ */
+/**
+ * Copyright (c) 2022 NurMarvin (Marvin Witt)
+ * Licensed under the Open Software License version 3.0
+ * Partial source code from https://github.com/NurMarvin/discord-proxy
+ */
+import fastify from "fastify";
 
-const app = express();
+import fastifyCors from "@fastify/cors";
+import fastifyHttpProxy from "@fastify/http-proxy";
+import fs from "fs/promises";
+import minimist from "minimist";
 
-const assetHost = 'https://canary.discord.com/assets/';
-const port = 3000;
+// handle cli arguments
+const args = minimist(process.argv.slice(2), {
+  alias: {
+    port: "p",
+    host: "h",
+    science: "s",
+    js: "j",
+    css: "c",
+    api: "a",
+  },
+  boolean: true,
+});
+const port = args.port ? parseInt(args.port) : 3000;
+const science = args.science ?? false;
+const jsFiles = (
+  args.js?.includes(",")
+    ? args.js.split(",")
+    : args.js
+)?.map(file => !file.includes(".js") ? `${file}.js` : file);
+const cssFile = args.css && !args.css.includes(".css") ? `${args.css}.css` : args.css;
+const apiVersion = args.api ?? 9;
 
-var science = false
+const canonicalUrl = new URL(args.host ?? `http://127.0.0.1:${port}`);
+const premadeEnv = {
+  API_ENDPOINT: `//${canonicalUrl.host}/api`,
+  API_VERSION: apiVersion,
+  WEBAPP_ENDPOINT: "//canary.discord.com",
+  GATEWAY_ENDPOINT: "wss://gateway.discord.gg",
+  CDN_HOST: "cdn.discordapp.com",
+  ASSET_ENDPOINT: "https://canary.discord.com",
+  MEDIA_PROXY_ENDPOINT: "https://media.discordapp.net",
+  WIDGET_ENDPOINT: "//canary.discord.com/widget",
+  INVITE_HOST: "discord.gg",
+  GUILD_TEMPLATE_HOST: "discord.new",
+  GIFT_CODE_HOST: "discord.gift",
+  RELEASE_CHANNEL: "canary",
+  MARKETING_ENDPOINT: "//canary.discord.com",
+  BRAINTREE_KEY: "production_5st77rrc_49pp2rp4phym7387",
+  STRIPE_KEY: "pk_live_CUQtlpQUF0vufWpnpUmQvcdi",
+  NETWORKING_ENDPOINT: "//router.discordapp.net",
+  RTC_LATENCY_ENDPOINT: "//latency.discord.media/rtc",
+  ACTIVITY_APPLICATION_HOST: "discordsays.com",
+  PROJECT_ENV: "production",
+  REMOTE_AUTH_ENDPOINT: "//remote-auth-gateway.discord.gg",
+  SENTRY_TAGS: {"buildId":"7ea92cf","buildType":"normal"},
+  MIGRATION_SOURCE_ORIGIN: "https://canary.discordapp.com",
+  MIGRATION_DESTINATION_ORIGIN: "https://canary.discord.com",
+  HTML_TIMESTAMP: Date.now(),
+  ALGOLIA_KEY: "aca0d7082e4e63af5ba5917d5e96bed0",
+  PUBLIC_PATH: "/assets/"
+};
 
-//handle process arguments
-const processArgs = process.argv.slice(2);
-if(processArgs.includes('-science')){
-  science = true
+const app = fastify();
+
+app.register(fastifyCors, {
+  origin: "*",
+});
+
+function getBuildFromArgs() {
+  if (!jsFiles || !cssFile) return null;
+  console.log("[Displunger] Launching from CLI arguments...")
+  return {
+    rootScripts: jsFiles,
+    css: cssFile,
+    buildNumber: "custom",
+    GLOBAL_ENV: {},
+  };
 }
 
-async function loadBuild(req, res){
-  try{
-    let build;
-    try{
-      build = await superagent.get(`https://api.discord.sale/builds/${req.params.build}`); //Fetch the build data
-    } catch(e){
-      res.status(400).send('Build not found.');
+async function fetchBuildFromDsale(buildId) {
+  try {
+    const res = await fetch(`https://api.discord.sale/builds/${buildId}`);
+    if (!build.ok) return null;
+
+    const build = await res.json();
+    console.log("[Displunger] Launching from Discord.sale API...", buildId)
+    return {
+      rootScripts: build.files.rootScripts.map(script => `${script}.js`),
+      css: `${build.files.css[0]}.css`,
+      buildNumber: build.number,
+      GLOBAL_ENV: build.GLOBAL_ENV,
     };
-    //pick the right app html template for build and inject scripts
-    let index;
+  } catch (e) {
+    return null;
+  }
+}
 
-    const rootScripts = build.body.files.rootScripts.map(script => script + ".js")
+async function fetchBuildFromGithub(buildId) {
+  try {
+    const builds = await fetch("https://raw.githack.com/Discord-Build-Logger/Builds/main/builds.json").then(r => r.json());
+    const { path } = builds[buildId];
 
-    switch(build.body.files.rootScripts.length){
+    const res = await fetch(`https://raw.githack.com/Discord-Build-Logger/Builds/main/${path}/${buildId}.json`);
+    if (!res.ok) return null;
+
+    const build = await res.json();
+    console.log("[Displunger] Launching from Discord-Build-Logger repo...", buildId)
+    return {
+      rootScripts: build.scripts.slice(0, 4).map(script => script.slice(8)),
+      css: cssFile,
+      buildNumber: buildId,
+      GLOBAL_ENV: build.GLOBAL_ENV,
+    };
+  } catch (e) {
+    return null;
+  }
+}
+
+async function fetchBuildData(res, buildId) {
+  const build = getBuildFromArgs() ?? await fetchBuildFromDsale(buildId) ?? await fetchBuildFromGithub(buildId);
+  if (build) {
+    return build;
+  } else {
+    res.status(400).send("Build not found.");
+  }
+}
+
+async function loadBuild(req, res) {
+  try {
+    const build = await fetchBuildData(res, req.params.build);
+    if (!build) return;
+
+    const { rootScripts, css, buildNumber, GLOBAL_ENV } = build;
+
+    // pick the right app html template for build and inject scripts
+    let body;
+
+    switch (rootScripts.length) {
       case 4:
-        index = fs.readFileSync('./app/modern.txt', 'utf-8');
-        index = index.replace(/\$LOADER/, rootScripts[0]);
-        index = index.replace(/\$CLASSES/, rootScripts[1]);
-        index = index.replace(/\$WEBPACK/, rootScripts[2]);
-        index = index.replace(/\$APP/, rootScripts[3]);
+        body = await fs.readFile("./app/modern.html", "utf-8");
+        body = body
+          .replace("$LOADER", rootScripts[0])
+          .replace("$CLASSES", rootScripts[1])
+          .replace("$WEBPACK", rootScripts[2])
+          .replace("$APP", rootScripts[3]);
         break;
       case 3:
-        index = fs.readFileSync('./app/old.txt', 'utf-8');
-        index = index.replace(/\$LOADER/, rootScripts[0]);
-        index = index.replace(/\$CLASSES/, rootScripts[1]);
-        index = index.replace(/\$APP/, rootScripts[2]);
+        body = await fs.readFile("./app/old.html", "utf-8");
+        body = body
+          .replace("$LOADER", rootScripts[0])
+          .replace("$CLASSES", rootScripts[1])
+          .replace("$APP", rootScripts[2]);
         break;
       default:
-        return res.status(400).send(`Unsupported Build Format (${rootScripts.length})`);
-        break;
+        return res
+          .status(400)
+          .send(`Unsupported Build Format (${rootScripts.length})`);
     };
-    //inject the stylesheet and other data
-    index = index.replace(/\$STYLE/g, build.body.files.css[0] + ".css");
-    index = index.replace(/\$BUILDID/g, build.body.number);
-    if(!build.body.GLOBAL_ENV.API_ENDPOINT){
-      var premadeEnv = {
-        API_ENDPOINT: '//discord.com/api',
-        WEBAPP_ENDPOINT: '//discord.com',
-        GATEWAY_ENDPOINT: 'wss://gateway.discord.gg',
-        CDN_HOST: 'cdn.discordapp.com',
-        ASSET_ENDPOINT: 'https://discord.com',
-        MEDIA_PROXY_ENDPOINT: 'https://media.discordapp.net',
-        WIDGET_ENDPOINT: '//discord.com/widget',
-        INVITE_HOST: 'discord.gg',
-        GUILD_TEMPLATE_HOST: 'discord.new',
-        GIFT_CODE_HOST: 'discord.gift',
-        RELEASE_CHANNEL: 'stable',
-        MARKETING_ENDPOINT: '//discord.com',
-        BRAINTREE_KEY: 'production_5st77rrc_49pp2rp4phym7387',
-        STRIPE_KEY: 'pk_live_CUQtlpQUF0vufWpnpUmQvcdi',
-        NETWORKING_ENDPOINT: '//router.discordapp.net',
-        RTC_LATENCY_ENDPOINT: '//latency.discord.media/rtc',
-        ACTIVITY_APPLICATION_HOST: 'discordsays.com',
-        PROJECT_ENV: 'production',
-        REMOTE_AUTH_ENDPOINT: '//remote-auth-gateway.discord.gg',
-        SENTRY_TAGS: {"buildId":"7ea92cf","buildType":"normal"},
-        MIGRATION_SOURCE_ORIGIN: 'https://discordapp.com',
-        MIGRATION_DESTINATION_ORIGIN: 'https://discord.com',
-        HTML_TIMESTAMP: Date.now(),
-        ALGOLIA_KEY: 'aca0d7082e4e63af5ba5917d5e96bed0',
-      }
-      if(science == true){
-        premadeEnv['RELEASE_CHANNEL'] = "staging"
-      }
-      index = index.replace(/\$GLOBALENV/, JSON.stringify(premadeEnv));
-    } else {
-      environment = build.body.GLOBAL_ENV
-      if(science == true){
-        environment['RELEASE_CHANNEL'] = "staging"
-      }
-      index = index.replace(/\$GLOBALENV/, JSON.stringify(environment));
-    };
-    //send the app
-    res.setHeader('Content-Type', 'text/html');
-    return res.send(index);
-  } catch(e){
+
+    // inject the stylesheet and other data
+    body = body
+      .replaceAll("$STYLE", css)
+      .replaceAll("$BUILDID", buildNumber);
+
+    // inject the global env
+    const environment = GLOBAL_ENV.API_ENDPOINT ? GLOBAL_ENV : premadeEnv;
+    if (science === true)
+      environment["RELEASE_CHANNEL"] = "staging";
+
+    body = body.replace("$GLOBALENV", JSON.stringify(environment));
+
+    // send the app
+    res.type("text/html").send(body);
+  } catch (e) {
     console.log(e);
-    return res.status(400).send('Failed to load build.');
+    res.status(400).send("Failed to load build.");
   }
 }
 
+// quick launch for latest version
+app.get("/launch", async (req, res) => {
+  const latest = await fetch(`https://api.discord.sale/builds`).then(r => r.json());
+  res.redirect(`/launch/${latest[0].hash}`);
+});
 
-//quick launch for latest version
-app.get('/launch', async function(req, res){
-  const latest = await superagent.get(`https://api.discord.sale/builds`);
-  res.redirect(`/launch/${latest.body[0].hash}`);
-})
+// main attraction
+app.get("/launch/:build/:endpoint", async (req, res) => {
+  await loadBuild(req, res);
+});
 
-//Main Attraction
-app.get('/launch/:build/:endpoint', async function(req, res){
-  loadBuild(req, res)
-})
+app.get("/launch/:build", async (req, res) => {
+  await loadBuild(req, res);
+});
 
-app.get('/launch/:build', async function(req, res){
-  loadBuild(req, res)
-})
+// proxy api requests
+app.all("/api/*", async (req, res) => {
+  const url = new URL(req.url, "https://canary.discord.com");
 
-//proxies all requests under /assets/*
-app.get('/assets/:asset', async function(req, res){
-  //return res.redirect(`${assetHost}${req.params.asset}`)   For the smart people, get a browser addon like CORS Anywhere and uncomment this line to redirect instead of proxying assets
-  try{
-    const site = await superagent.get(`${assetHost}${req.params.asset}`);
-    res.setHeader('Content-Type', site.headers['content-type']);
-    if(req.params.asset.endsWith('css')){ return res.send(site.text); };
-    //fix for old builds
-    if(req.params.asset.endsWith('js')){
-      body = site.body
-      return res.send(body.toString().replace(/r\[t.type\].push/g, 'r[t.type]?.push'))
-    }
-    return res.send(site.body);
-  } catch(e){
-    //console.log(e)  -  Uncomment for debugging
-    res.status(404).send('Not Found.');
-  };
-})
+  const headers = Object.fromEntries(
+    Object.entries(req.headers)
+      .filter(([key]) => key !== "referer" && key !== "connection")
+      .map(([key, value]) => [
+        key,
+        key === "host" ? url.host : key === "origin" ? url.origin : value,
+      ])
+  );
 
-app.get('*', async function(req, res){
-  return res.status(404).send('displunger | Visit /launch/&lt;build hash&gt; to launch a build.');
-})
+  const response = await fetch(url, {
+    method: req.method,
+    headers,
+    body: req.body ? JSON.stringify(req.body) : undefined,
+  });
 
-app.listen(port, () => {
-  if(science == true){
-    console.log('[Displunger] displunger ready with science.')
-  } else {
-    console.log(`[Displunger] displunger ready.`);
+  res
+    .status(response.status)
+    .headers(response.headers)
+    .type(response.headers.get("content-type"))
+    .send(await response.text());
+});
+
+app.get("*", async (req, res) => {
+  res.send("displunger | Visit /launch/<build hash> to launch a build.");
+});
+
+// proxy assets
+app.register(fastifyHttpProxy, {
+  upstream: "https://canary.discord.com",
+  prefix: "/assets",
+  rewritePrefix: "/assets",
+});
+
+app.listen({ port, host: canonicalUrl.hostname }, (err) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
   }
-  console.log(`[Displunger] Launch a build via \x1b[36m\x1b[4mhttp://localhost:${port}/launch/<build hash>\x1b[0m`);
+
+  if (science === true)
+    console.log("[Displunger] displunger ready with science.")
+  else
+    console.log(`[Displunger] displunger ready.`);
+
+  console.log(`[Displunger] Launch a build via \x1b[36m\x1b[4m${canonicalUrl}launch/<build hash>\x1b[0m`);
 });
